e65261f2bd88c55dea6b4744682125d9e2d5b58f
thejus-ms <thejus.sm08@gmail.com>
2022-03-28 12:59:44 -0400

    old project

---
+++ listings/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+from .views import ListingsListView
+from .views import ListingsDetailView
+
+urlpatterns = [
+    # path('/li',views.listing,name='list'),
+    path('', ListingsListView.as_view(), name='listings'),
+    path('<int:pk>/', ListingsDetailView.as_view(), name='listing'),
+    path('search/',views.search,name='search')
+]
